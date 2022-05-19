7c803e9a21344ed9e9533eda51325336795c25eb
najuml-bd <najmul0612@gmail.com>
2022-05-19 18:37:58 +0600

    verify admin function is created

--- index.js
+++ index.js
@@ -38,6 +38,18 @@ async function run() {
         const serviceCollection = client.db('doctors-portal').collection('services');
         const bookingCollection = client.db('doctors-portal').collection('booking');
         const userCollection = client.db('doctors-portal').collection('user');
+        const doctorCollection = client.db('doctors-portal').collection('doctors');
+
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester })
+            if (requesterAccount.role === "admin") {
+                next()
+            } else {
+                res.status(403).send({ message: 'Forbidden Access' })
+            }
+
+        }
 
         app.get('/services', async (req, res) => {
             const query = {};
@@ -103,22 +115,14 @@ async function run() {
         })
 
 
-        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
             const email = req.params.email;
-            const requester = req.decoded.email;
-            const requesterAccount = await userCollection.findOne({ email: requester })
-            if (requesterAccount.role === "admin") {
-                const filter = { email: email };
-                const updateDoc = {
-                    $set: { role: 'admin' },
-                };
-                const result = await userCollection.updateOne(filter, updateDoc)
-                res.send(result);
-            } else {
-                res.status(403).send({ message: 'Forbidden Access' })
-            }
-
-
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc)
+            res.send(result);
         })
 
         app.put('/user/:email', async (req, res) => {
@@ -137,6 +141,12 @@ async function run() {
 
         })
 
+        // add a doctor
+        app.post('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
+            const doctor = req.body;
+            const result = await doctorCollection.insertOne(doctor);
+            res.send(result);
+        })
 
         // insert a data to the data base
         app.post('/booking', async (req, res) => {
