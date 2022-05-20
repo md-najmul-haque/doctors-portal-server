90778cb864882536a8fa452849a08c59859271d4
najuml-bd <najmul0612@gmail.com>
2022-05-20 12:22:02 +0600

    nodemailer and sendgrid added for sending mail

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const { MongoClient, ServerApiVersion, Admin } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+var nodemailer = require('nodemailer');
+var sgTransport = require('nodemailer-sendgrid-transport');
+
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -30,6 +33,47 @@ function verifyJWT(req, res, next) {
         req.decoded = decoded;
         next();
     });
+}
+
+const emailSenderOptions = {
+    auth: {
+        api_key: process.env.EMAIL_SENDER_KEY
+    }
+}
+
+const emailClient = nodemailer.createTransport(sgTransport(emailSenderOptions));
+
+function sendAppointmentEmail(booking) {
+    const { patient, patientName, treatment, date, slot } = booking;
+
+    var email = {
+        from: process.env.EMAIL_SENDER,
+        to: patient,
+        subject: `Your Appointment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+        text: `Your Appointment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+        html: `
+        <div>
+          <p> Hello ${patientName}, </p>
+          <h3>Your Appointment for ${treatment} is confirmed</h3>
+          <p>Looking forward to seeing you on ${date} at ${slot}.</p>
+          
+          <h3>Our Address</h3>
+          <p>Andor Killa Bandorban</p>
+          <p>Bangladesh</p>
+          <a href="https://web.programming-hero.com/">unsubscribe</a>
+        </div>
+      `
+    };
+
+    emailClient.sendMail(email, function (err, info) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log('Message sent: ', info);
+        }
+    });
+
 }
 
 async function run() {
@@ -168,8 +212,12 @@ async function run() {
             if (exist) {
                 return res.send({ success: false, booking: exist })
             }
-
             const result = await bookingCollection.insertOne(booking);
+
+            console.log('sending email');
+            sendAppointmentEmail(booking);
+            return res.send({ success: true, result });
+
             res.send({ success: true, result });
 
         })
